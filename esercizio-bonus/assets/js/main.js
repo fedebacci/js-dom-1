42c2f97ea28edf5c5a32a79f456d3e6fe2db2132
fedebacci <federicobacci1999@gmail.com>
2025-04-09 18:01:45 +0200

    implementazione soluzione samuel con toggle delle classi

--- esercizio-bonus/assets/js/main.js
+++ esercizio-bonus/assets/js/main.js
@@ -4,15 +4,24 @@ const lightBulbImage = document.querySelector('img');
 console.debug('lightBulbImage', lightBulbImage);
 const lightBulbCSSElement = document.getElementById('bulb');
 console.debug('lightBulbCSSElement', lightBulbCSSElement);
+const imgWhiteLamp = document.getElementById('imgWhiteLamp');
+console.debug('lightBulbCSSElement', lightBulbCSSElement);
+const imgYellowLamp = document.getElementById('imgYellowLamp');
+console.debug('lightBulbCSSElement', lightBulbCSSElement);
+const CSSlightbulb = document.getElementById('CSSlightbulb');
+console.debug('lightBulbCSSElement', lightBulbCSSElement);
 
 switchButton.addEventListener('click', function() {
-    switchLight(switchButton, lightBulbImage, lightBulbCSSElement)
+    switchLight(switchButton, lightBulbImage, lightBulbCSSElement, imgWhiteLamp, imgYellowLamp, CSSlightbulb)
 });
 
-function switchLight(button, image, CSSelement) {
+function switchLight(button, image, CSSelement, imgWhite, imgYellow, CSSlightbulb) {
     // console.debug('button', button);
     // console.debug('image', image);
     // console.debug('CSSelement', CSSelement);
+    // console.debug('imgWhite', imgWhite);
+    // console.debug('imgYellow', imgYellow);
+    // console.debug('CSSlightbulb', CSSlightbulb);
 
     if (image.src.includes('white')) {
         console.info("Accendo")
@@ -20,13 +29,17 @@ function switchLight(button, image, CSSelement) {
         image.alt = 'Lampadina accesa';
         CSSelement.style.backgroundColor = 'yellow';
         CSSelement.style.boxShadow = '0 0 5rem 1px rgba(255, 255, 0, 1)';
-        button.innerText = 'Spegni la lampadina';
+        button.innerText = 'Spegni le lampadine';
     } else {
         console.info("Spengo")
         image.src = './assets/img/white_lamp.png';
         image.alt = 'Lampadina spenta';
         CSSelement.style.backgroundColor = 'white';
         CSSelement.style.boxShadow = '0 0 2rem 1px rgba(0,0,0,0.25)';
-        button.innerText = 'Accendi la lampadina';
+        button.innerText = 'Accendi le lampadine';
     };
+
+    imgWhite.classList.toggle('d-none');
+    imgYellow.classList.toggle('d-none');
+    CSSlightbulb.classList.toggle('lit');
 };
