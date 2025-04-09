fcd4c9a54a2e9ab49cd75fd831fb2b51a4676a21
fedebacci <federicobacci1999@gmail.com>
2025-04-09 15:26:39 +0200

    svolgimento esercizio base

---
+++ esercizio-base/assets/js/main.js
@@ -0,0 +1,9 @@
+const btnTurnOn = document.getElementById('turnOn');
+console.debug('btnTurnOn', btnTurnOn);
+
+btnTurnOn.addEventListener('click', () => {
+    const lightBulbImage = document.querySelector('img');
+    console.debug('lightBulbImage', lightBulbImage);
+    lightBulbImage.src = './assets/img/yellow_lamp.png';
+    lightBulbImage.alt = 'Lampadina accesa';
+});
